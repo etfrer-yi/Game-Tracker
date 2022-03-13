b94d2d604142d81c4450ab18e81b5c90e0c0d71b
Yi Heng <yihengwang2000@gmail.com>
2022-03-12 23:50:15 -0500

    Have the webscraper working for Battlefield 1 Tracker, able to generate a JSON with the yielded player info

--- game_tracker_scraper/game_tracker_scraper/spiders/battlefield1_scraper.py
+++ game_tracker_scraper/game_tracker_scraper/spiders/battlefield1_scraper.py
@@ -1,38 +1,71 @@
 import scrapy
 
+DOMAIN = "https://battlefieldtracker.com"
+
+
 class Battlefield1Scraper(scrapy.Spider):
     name = "battlefield1"
 
     def start_requests(self):
-        # start_urls = ["https://battlefieldtracker.com/bf1/leaderboards/all/SiteScore"]
-        # start_urls += [
-        #     f"https://battlefieldtracker.com/bf1/leaderboards/all/SiteScore?page={page}"
-        #     for page in range(2, 51)
-        # ]
-        urls = [
-            "https://battlefieldtracker.com/bf1/leaderboards/all/SiteScore"
+        urls = ["https://battlefieldtracker.com/bf1/leaderboards/all/SiteScore"]
+        urls += [
+            f"https://battlefieldtracker.com/bf1/leaderboards/all/SiteScore?page={page}"
+            for page in range(2, 5)
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_scoreboard)
 
-    def parse(self, response):
+    def parse_scoreboard(self, response):
         for idx, player_record in enumerate(response.css("tr")):
             player_record_fields = player_record.css('td').getall()
-            player_record_fields
             if len(player_record_fields) == 4:
-                yield {
-                    'Rank': int(player_record.css('td::text').getall()[0]),
+                player_info = {
+                    'Rank': self.convert_numerical_str_to_num(
+                        player_record.css('td::text').getall()[0].replace(",", "")),
                     'Gamer': player_record.css('td')[1].css("a::text").get(),
-                    'BTR Score': int(player_record.css('td')[2].css("div.pull-right::text").get()
-                                     .replace("\r\n", "").replace(",", "").strip()),
-                    'Games': int(player_record.css('td::text').getall()[-1])
+                    'BTR Score': self.convert_numerical_str_to_num(
+                        player_record.css('td')[2].css("div.pull-right::text").get()),
+                    'Games': self.convert_numerical_str_to_num(player_record.css('td::text').getall()[-1])
                 }
-                print(1)
+                player_profile_url = player_record.css("td")[1].css("a::attr(href)").get()
+                if player_profile_url is not None:
+                    player_profile_url = DOMAIN + player_profile_url
+                    yield scrapy.Request(player_profile_url, callback=self.parse_player_profile,
+                                         meta={'player_info': player_info})
 
+    def parse_player_profile(self, response):
+        if response.css(
+                ".alert"):  # sometimes the website has trouble downloading data due to exceeding INT_MAX, so an alert shows up
+            return
 
+        relevant_stats = response.css("div.stats div.stat div.value::text").getall()
+        player_info = response.meta.get("player_info")
+        yield {
+            'Rank': player_info['Rank'],
+            'Gamer': player_info['Gamer'],
+            'BTR Score': player_info['BTR Score'],
+            'Games': player_info['Games'],
+            'Score/min': self.convert_numerical_str_to_num(relevant_stats[1]),
+            'Kill Ratio': self.convert_numerical_str_to_num(relevant_stats[2]),
+            'Win Percent': self.convert_numerical_str_to_num(relevant_stats[3]) / 100,
+            'Game Score': self.convert_numerical_str_to_num(relevant_stats[4]),
+            'Hours Played': self.get_hours_from_play_time(relevant_stats[5]),
+        }
 
-# For a row of valid record,
-#  player_record.css('td') returns 4 Selectors
-#  player_record.css('td').getall() returns 4 innerHTMLs (kind of) as str
-#  player_record.css('td::text') returns 7 Selectors, potentially the td's along with their children
-#  player_record.css('td::text').getall() returns 7 selectors but with the innerHTMLs (kind of) as str
+    def convert_numerical_str_to_num(self, numerical_str):
+        return round(float(numerical_str.replace("\r\n", "").replace(",", "").replace("%", "").strip()), 3)
+
+    def get_hours_from_play_time(self, play_time):
+        play_time = play_time.split()
+        hours = 0
+        for day_hour_min_sec_type in play_time:
+            time_value = day_hour_min_sec_type[:-1]  # excludes the last character, which is 'd', 'h', 'm', or 's'
+            if "d" in day_hour_min_sec_type:
+                hours += 24 * float(time_value)
+            elif "h" in day_hour_min_sec_type:
+                hours += float(time_value)
+            elif "m" in day_hour_min_sec_type:
+                hours += float(time_value) / 60
+            elif "s" in day_hour_min_sec_type:
+                hours += float(time_value) / 3600
+        return round(hours, 3)
--- game_tracker_scraper/main.py
+++ game_tracker_scraper/main.py
@@ -1,2 +1,4 @@
+# This file is only meant to be run in PYCHARM Run/Debug configurations
+
 from scrapy import cmdline
 cmdline.execute("scrapy crawl battlefield1".split())
