7682f6f6e60a5c415e6f6f1f9db36830a747c8f0
Yi Heng <yihengwang2000@gmail.com>
2022-03-15 20:23:31 -0400

    Solve the UTF-8 encoding issue due to inspectdb not generating models.py in UTF-8 encoding

--- gametracker/settings.py
+++ gametracker/settings.py
@@ -36,14 +36,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'battlefield1',
+    'battlefield4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'battlefield1',
-    'battlefield4'
 ]
 
 MIDDLEWARE = [
